820b2b6c636a55b91d5678cce024f6c0eedd5b0d
rchoquet42 <rchoquet@student.42.fr>
2019-02-27 16:14:00 +0100

    init/script mv 03.sh 03

--- init/scripts/02.sh
+++ init/scripts/02.sh
@@ -1,23 +1,30 @@
+#!bin/bash
 if [ "$EUID" -ne 0 ]; then 
 	echo "Please run as root"
-	exit
+	exit 1
+fi;
 if [ $# -ne 1 ]; then
 	echo "USAGE: 02 user_name"
-	exit
+	exit 1
+fi;
 passwd -l $1 > /dev/null 
 if [ $? -ne 0 ]; then
 	echo "FAILED TO DELETE USER" $1
-	exit
+	exit 1
+fi;
 pkill -KILL -u $1 > /dev/null
 if [ $? -ne 0 ]; then
 	echo "FAILED TO DELETE USER" $1
-	exit
+	exit 1
+fi;
 userdel -r $1 > /dev/null
 if [ $? -ne 0 ]; then
 	echo "FAILED TO DELETE USER" $1
-	exit
+	exit 1
+fi;
 crontab -r -u $1 > /dev/null
 if [ $? -ne 0 ]; then
 	echo "FAILED TO DELETE USER" $1
-	exit
+	exit 1
+fi;
 echo "$1 SUCCESFULLY DELETED"
